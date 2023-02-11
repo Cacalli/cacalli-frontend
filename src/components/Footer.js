4dfc1bd6295476093331ead6a2d81c64d10b7340
jessica <jessicahercon@gmail.com>
2023-02-10 21:56:46 -0600

    changes on footer deleted reinventing cycles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,32 +2,35 @@ import { Link } from "react-router-dom"
 import {FaInstagram, FaWhatsapp} from 'react-icons/fa';
 
 export default function Footer(){
+  return (
+    <footer className="bg-blue-200 text-black justify-center py-14">
+      <div className="mb-6 grid grid-cols-4 gap-4 px-32 ">
+        <Link className="">
+          <img className="w-58 h-48" src="/assets/logoCacalli.png" />
+        </Link>
 
-    return(
-        <footer className="bg-blue-200 text-black h-10 flex justify-center mt-8">
-            <Link className="mx-28">Logo cacalli home</Link>
-                <div className="flex flex-col mx-28 text-neutral-gray-two">
-                    <p className="font-bold ">Acerca de</p>
-                    <Link className="my-6">¿Quiénes somos?</Link>
-                    <Link className="my-6">¿Qué hacemos?</Link>
-                    <Link className="my-6">¿Por qué Cacalli?</Link>
-                    <Link className="my-6">Quiero contratar</Link>
-                </div>
+        <div className="text-neutral-gray-two align-start">
+          <p className="font-bold ">Acerca de</p>
+          <Link className="my-2 block">¿Quiénes somos?</Link>
+          <Link className="my-2 block">¿Qué hacemos?</Link>
+          <Link className="my-2 block">¿Por qué Cacalli?</Link>
+          <Link className="my-2 block">Quiero contratar</Link>
+        </div>
 
-                <div className="flex flex-col mx-28 text-neutral-gray-two">
-                    <p className="font-bold ">Contacto</p>
-                    <Link className="my-6">Términos y condiciones</Link>
-                    <Link className="my-6">Política de privacidad</Link>
-                </div>
+        <div className="  text-neutral-gray-two ">
+          <p className="font-bold ">Contacto</p>
+          <Link className="my-2 block">Términos y condiciones</Link>
+          <Link className="my-2 block">Política de privacidad</Link>
+        </div>
 
-                <div className="flex flex-col mx-28 text-neutral-gray-two">
-                    <p className="font-bold">Redes sociales</p>
-                    <div className="flex my-6 m-0 text-green-three">
-                        <FaInstagram  size="1.5em"/>
-                        <FaWhatsapp className="mx-3" size="1.5em"/>
-                    </div>
-
-                </div>
-        </footer>
-    )
+        <div className="text-neutral-gray-two ">
+          <p className="font-bold">Redes sociales</p>
+          <div className="flex mt-6 px-44 text-green-three">
+            <FaInstagram size="1.5em" />
+            <FaWhatsapp className="mx-3" size="1.5em" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
