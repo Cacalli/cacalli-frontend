f30eeb63901438abd91fa0ddb2af6019d0513576
jessica <jessicahercon@gmail.com>
2023-02-20 17:26:49 -0600

    video section edited

--- src/components/VideoContainer/VideoContainer.js
+++ src/components/VideoContainer/VideoContainer.js
@@ -1,28 +1,21 @@
 export default function VideoContainer() {
   return (
-    // <div className="py-4 bg-green-one flex flex-col ">
-    //
-    // </div>
-    <div className="bg-green-one py-40">
-      <div className="w-10/12 flex mx-auto">
-        <div className="flex w-full items-center">
-          <div className="flex-1">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/OA30NLDQQMY"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <div className="flex-1">
-            <div className="mb-6">
-              <p className="text-7xl text-neutral-white font-lovelo">VIDEO</p>
-            </div>
-          </div>
-        </div>
+    <div className="flex  bg-green-one py-40">
+      <div className=" md:flex flex-1 justify-center">
+        <iframe
+          width="700"
+          height="350"
+          src="https://www.youtube.com/embed/OA30NLDQQMY"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="md:flex flex-1  mb-6 items-center ">
+        <p className="text-5xl md:text-7xl text-neutral-white font-lovelo pl-24">
+          Conoce Cacalli
+        </p>
       </div>
     </div>
   );
