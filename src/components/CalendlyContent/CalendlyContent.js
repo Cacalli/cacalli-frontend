c911ec0467d45f0459badab4f6e4e8de7776a0c0
jessica <jessicahercon@gmail.com>
2023-03-21 19:47:41 -0600

    modifying route in button from Calendly.js

--- src/components/CalendlyContent/CalendlyContent.js
+++ src/components/CalendlyContent/CalendlyContent.js
@@ -1,10 +1,15 @@
 import { InlineWidget } from "react-calendly";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 export default function CalendlyContent() {
   return (
-    <div>
+    <div className="">
       <InlineWidget url="https://calendly.com/cacalli/10min" />
-      <Link to="/dashboad">Volver al dashboard</Link>
+      <Link to="/dashboard">
+        <Button variant="primary" className="ml-8">
+        Volver al dashboard
+        </Button>
+      </Link>
     </div>
   );
 }
